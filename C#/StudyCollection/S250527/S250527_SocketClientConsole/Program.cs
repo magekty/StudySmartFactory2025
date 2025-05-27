a67cce3196605de43651569e3e7ff1137892d19d
magekty <72486918+magekty@users.noreply.github.com>
2025-05-28 06:44:18 +0900

    250528HomeStudy

--- C#/StudyCollection/S250527/S250527_SocketClientConsole/Program.cs
+++ C#/StudyCollection/S250527/S250527_SocketClientConsole/Program.cs
@@ -23,10 +23,10 @@ namespace S250527_SocketClientConsole
                 while (true)
                 {
                     // 사용자 입력 처리 루프 : 내일 오전에 계속
-                    string message = Console.ReadLine();
+                    string message = Console.ReadLine();    // 콘솔 입력값 받기
                     //if (string.IsNullOrWhiteSpace(message)) continue;
-                    byte[] data = Encoding.UTF8.GetBytes(message);
-                    await stream.WriteAsync(data, 0, data.Length);
+                    byte[] data = Encoding.UTF8.GetBytes(message);  // 입력값을 Byte배열로 인코딩
+                    await stream.WriteAsync(data, 0, data.Length);    // stream에 비동기쓰기(쓰기값,시작,끝)
                 }
             }
             catch (Exception ex)
@@ -43,20 +43,23 @@ namespace S250527_SocketClientConsole
 
         static async Task ReceiveMessageAsync(NetworkStream stream)
         {
-            byte[] buffer = new byte[1024];
+            int bufferLength = 1024;
+            byte[] buffer = new byte[bufferLength];
             int bytesRead = 0;
             try
             {
                 while (true)
                 {
                     // 내일 채울 것
-                    if((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) == 17) Console.Write("입력: ");
-                    //if(bytesRead == 17) Console.Write("입력: ");
-                    if (bytesRead == 17) continue;
-                    string received = Encoding.UTF8.GetString(buffer);
+                    // 버퍼에서읽은Bytes의총량= 비동기읽기(읽고담을변수,시작,끝) 
+                    if ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != bufferLength) 
+                        Console.Write("입력: ");
+                    if (bytesRead != bufferLength) continue;    // 버퍼Bytes의총량이 버퍼최대 길이와 다를때
+                    string received = Encoding.UTF8.GetString(buffer);  // 버퍼값을 문자열로 인코딩
+                    int origRow = Console.CursorTop;    // 현재 커서 Y축(행) 위치값 반환
+                    int origCol = Console.CursorLeft;    // 현재 커서 X축(열) 위치값 반환
+                    Console.SetCursorPosition(0, origRow);  // 콘솔 창에서 다음 쓰기 작업을 시작할 위치를 지정
                     Console.WriteLine($"[서버] {received}");
-                    /*Console.WriteLine($"byte:{bytesRead}");
-                    Console.WriteLine($"buffer:{buffer.Length}");*/
                 }
             }
             catch (IOException ex)
