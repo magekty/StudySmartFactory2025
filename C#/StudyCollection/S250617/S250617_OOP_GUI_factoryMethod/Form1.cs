f2d27a4a07c2153fecdc9bac32b4b6df876aee5a
magekty <72486918+magekty@users.noreply.github.com>
2025-06-17 17:11:04 +0900

    S250617_StudyEnd
    End

---
+++ C#/StudyCollection/S250617/S250617_OOP_GUI_factoryMethod/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S250617_OOP_GUI_factoryMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLightTheme_Click(object sender, EventArgs e)
+        {
+            ThemeFactory factory = new LightThemeFactory();
+            BaseThemeForm form = factory.CreateThemeForm();
+            form.Show();
+        }
+
+        private void btnDarkTheme_Click(object sender, EventArgs e)
+        {
+            ThemeFactory factory = new DarkThemeFactory();
+            BaseThemeForm form = factory.CreateThemeForm();
+            form.Show();
+        }
+    }
+
+    abstract class BaseThemeForm : Form
+    {
+        protected BaseThemeForm()
+        {
+            this.Width = 300;
+            this.Height = 200;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+    }
+    class LightThemeForm : BaseThemeForm
+    {
+        public LightThemeForm()
+        {
+            this.Text = "Light Theme";
+            this.BackColor = Color.White;
+            Label lbl = new Label
+            {
+                Text = "Light Theme",
+                ForeColor = Color.White,
+                AutoSize = true,
+                Location = new Point(50, 80)
+            };
+            Controls.Add(lbl);
+        }
+    }
+    class DarkThemeForm : BaseThemeForm
+    {
+        public DarkThemeForm()
+        {
+            this.Text = "Dark Theme";
+            this.BackColor = Color.Black;
+            Label lbl = new Label
+            {
+                Text = "Dark Theme",
+                ForeColor = Color.Black,
+                AutoSize = true,
+                Location = new Point(50, 80)
+            };
+            Controls.Add(lbl);
+        }
+    }
+    abstract class ThemeFactory
+    {
+        public abstract BaseThemeForm CreateThemeForm();
+    }
+    class LightThemeFactory : ThemeFactory
+    {
+        public LightThemeFactory()
+        {
+        }
+
+        public override BaseThemeForm CreateThemeForm()
+        {
+            return new LightThemeForm();
+        }
+    }
+    class DarkThemeFactory : ThemeFactory
+    {
+        public DarkThemeFactory()
+        {
+        }
+
+        public override BaseThemeForm CreateThemeForm()
+        {
+            return new DarkThemeForm();
+        }
+    }
+}
---
+++ React/SelfStudy/ReactStart/firstAppTest1/src/main.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import MyForm from "./chapter08/MyForm.jsx";
+import MyList from "./chapter08/MyList.jsx";
+import MyTable from "./chapter08/MyTable.jsx";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+// root.render(<App />);
+root.render(<MyForm />);
+// root.render(<MyList />);
+// root.render(<MyTable />);
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//     {/* <MyForm />
+//     <MyList /> */}
+//     <MyTable />
+//   </StrictMode>
+// );
